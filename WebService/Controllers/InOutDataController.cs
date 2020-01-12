103630d973afb502b698e6044d4a202a3142999d
Kirill <kislov-k@inbox.ru>
2020-01-12 20:08:52 +0300

    [KislovK]Практическое задание 1 без записи в фаил (данные просто висят в памяти(и то в рамках одного запроса(забыл синглтон какой нибудь сделать))

---
+++ WebService/Controllers/InOutDataController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using WebService.Utilit;
+
+namespace WebService.Controllers
+{
+    [ApiController]
+    [Route("data")]
+    public class InOutDataController : ControllerBase
+    {
+        private readonly ILogger<InOutDataController> _logger;
+        private readonly IOptionsMonitor<CheckWords> _checkWords;
+
+        //Пока на памяти храним, потом базенку прикручу
+        private List<string> _data = new List<string>();
+        
+        public InOutDataController(ILogger<InOutDataController> logger,
+            IOptionsMonitor<CheckWords> options)
+        {
+            _logger = logger;
+            _checkWords = options;
+        }
+
+        /// <summary>
+        /// Получение всех слов
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("all")]
+        public IEnumerable<string> GetAll() => _data;
+
+        /// <summary>
+        /// Добавление слова по индексу
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        [HttpPost("insert/one")]
+        public IActionResult InsertItem([FromBody]string word)
+        {
+            if (!CheckWord(word))
+            {
+                _logger.LogWarning("Данное слово не приветствие!");
+                return BadRequest("Данное слово не приветствие!");
+            }
+
+            _data.Add(word.ToLower());
+            return Ok();
+        }
+
+        /// <summary>
+        /// Получение всех слов по индексу
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        [HttpGet("from/index")]
+        public string Get(int index) => _data.ElementAtOrDefault(index) ?? "Такой индекс отсутствует!";
+
+        /// <summary>
+        /// Удаление слова по индексу
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        [HttpDelete("from/index")]
+        public IActionResult DeleteData(int index)
+        {
+            if (_data.Count >= index - 1)
+            {
+                _data.RemoveAt(index);
+                return Ok();
+            }
+
+            _logger.LogError("Данный индекс отутствует");
+            return BadRequest("Данный индекс отутствует");
+        }
+
+        /// <summary>
+        /// Обновление слова по индексу
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        [HttpPut("update")]
+        public IActionResult UpdateData(int index, string word)
+        {
+            if (_data.Count >= index - 1 && CheckWord(word))
+            {
+                _data[index - 1] = word;
+                return Ok();
+            }
+
+            return BadRequest("Слово не приветствие, либо такого индекса не существует!");
+        }
+
+        /// <summary>
+        /// Проверка слова на слова приветствия
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        private bool CheckWord(string word) => _checkWords.CurrentValue?.WordList.Any(x => x == word.ToLower()) ?? false;
+    }
+}
--- WebService/Startup.cs
+++ WebService/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using WebService.Utilit;
 
 namespace WebService
 {
@@ -22,13 +16,13 @@ namespace WebService
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CheckWords>(Configuration.GetSection("CheckWords"));
+            //services.AddOptions<CheckWords>();
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
